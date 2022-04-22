d8c357e8f8ad2bf337db3a1bc9254b8eb643740f
gbeletti <gbeletti@gmail.com>
2022-04-22 18:39:11 -0300

    publish function

---
+++ publish.go
@@ -0,0 +1,26 @@
+package rabbitmq
+
+import (
+	"context"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func (r *rabbit) Publish(ctx context.Context, body []byte, config ConfigPublish) (err error) {
+	r.wgChannel.Add(1)
+	defer r.wgChannel.Done()
+	err = r.chProducer.Publish(
+		config.Exchange,
+		config.RoutingKey,
+		config.Mandatory,
+		config.Immediate,
+		amqp.Publishing{
+			Headers:       config.Headers,
+			ContentType:   config.ContentType,
+			Priority:      config.Priority,
+			CorrelationId: config.CorrelationID,
+			Body:          body,
+		},
+	)
+	return
+}
