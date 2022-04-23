b4d0665eeb444dd1a3d04f4d609fd3b4d1c0cf2a
gbeletti <gbeletti@gmail.com>
2022-04-23 17:06:41 -0300

    - adds connection open notification - adds keep connection and setup function - adds setup notification

--- connection.go
+++ connection.go
@@ -3,11 +3,18 @@ package rabbitmq
 import (
 	"context"
 	"log"
+	"sync"
+	"time"
 
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
-// Connect connects to the rabbitMQ server and also creates the channels to produce and consume messages
+var notifyOpenConn, notifySetupDone []chan struct{}
+var muxNotifyOpenConn, muxNotifySetup sync.Mutex = sync.Mutex{}, sync.Mutex{}
+
+// Connect connects to the rabbitMQ server and also creates the channels to produce and consume messages.
+// It can also notify the connection is open to other goroutines if the function NotifyOpenConnection
+// is called before connecting.
 func (r *rabbit) Connect(config ConfigConnection) (notify chan *amqp.Error, err error) {
 	notify = make(chan *amqp.Error)
 	r.conn, err = amqp.Dial(config.URI)
@@ -25,7 +32,11 @@ func (r *rabbit) Connect(config ConfigConnection) (notify chan *amqp.Error, err 
 	}
 	if config.PrefetchCount > 0 {
 		err = r.chConsumer.Qos(config.PrefetchCount, 0, false)
+		if err != nil {
+			return
+		}
 	}
+	notifyOpenConnections()
 	return
 }
 
@@ -48,6 +59,60 @@ func (r *rabbit) Close(ctx context.Context) (done chan struct{}) {
 		closeConnections(r)
 	}()
 	return
+}
+
+// KeepConnectionAndSetup starts a goroutine to keep the connection open and everytime the connection is open, it will call the setupRabbit function. It is important to pass a context with cancel so the goroutine can be closed when the context is done. Otherwise it will run until the program ends.
+func KeepConnectionAndSetup(ctx context.Context, conn Connector, config ConfigConnection, setupRabbit RabbitSetup) {
+	go func() {
+		for {
+			notifyClose, err := conn.Connect(config)
+			if err != nil {
+				log.Printf("error connecting to rabbitmq: [%s]\n", err)
+				time.Sleep(time.Second * 5)
+				continue
+			}
+			setupRabbit.Setup()
+			notifySetupIsDone()
+			select {
+			case <-notifyClose:
+				continue
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+}
+
+// NotifyOpenConnection registers a channel to be notified when the connection is open
+func NotifyOpenConnection(notify chan struct{}) {
+	muxNotifyOpenConn.Lock()
+	defer muxNotifyOpenConn.Unlock()
+	notifyOpenConn = append(notifyOpenConn, notify)
+}
+
+// NotifySetupDone registers a channel to be notified when the setup is done by the KeepConnectionAndSetup function
+func NotifySetupDone(notify chan struct{}) {
+	muxNotifySetup.Lock()
+	defer muxNotifySetup.Unlock()
+	notifySetupDone = append(notifySetupDone, notify)
+}
+
+func notifyOpenConnections() {
+	muxNotifyOpenConn.Lock()
+	defer muxNotifyOpenConn.Unlock()
+	for _, notify := range notifyOpenConn {
+		close(notify)
+	}
+	notifyOpenConn = make([]chan struct{}, 0)
+}
+
+func notifySetupIsDone() {
+	muxNotifySetup.Lock()
+	defer muxNotifySetup.Unlock()
+	for _, notify := range notifySetupDone {
+		close(notify)
+	}
+	notifySetupDone = make([]chan struct{}, 0)
 }
 
 func closeConnections(r *rabbit) {
