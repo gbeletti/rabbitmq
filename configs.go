1b9483e99461c503bd15fb8871aee34b3468d98d
gbeletti <gbeletti@gmail.com>
2022-04-28 21:50:13 -0300

    adds some helper functions to get configuration structs

--- configs.go
+++ configs.go
@@ -52,3 +52,31 @@ type ConfigPublish struct {
 	Priority      uint8
 	CorrelationID string
 }
+
+// NewConfigConsume helper function to create a new ConfigConsume with some default values
+func NewConfigConsume(queueName, consumer string) ConfigConsume {
+	return ConfigConsume{
+		QueueName:         queueName,
+		Consumer:          consumer,
+		AutoAck:           false,
+		Exclusive:         false,
+		NoLocal:           false,
+		NoWait:            false,
+		Args:              nil,
+		ExecuteConcurrent: true,
+	}
+}
+
+// NewConfigPublish helper function to create a new ConfigPublish with some default values
+func NewConfigPublish(exchange, routingKey string) ConfigPublish {
+	return ConfigPublish{
+		Exchange:      exchange,
+		RoutingKey:    routingKey,
+		Mandatory:     false,
+		Immediate:     false,
+		Headers:       nil,
+		ContentType:   "",
+		Priority:      0,
+		CorrelationID: "",
+	}
+}
